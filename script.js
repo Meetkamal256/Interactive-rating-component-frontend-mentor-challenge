9a5bff923f711f1c511af7f6c4b8712ac426b9c6
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-09-04 22:43:37 +0100

    added  javascript code to submit button

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const 
+const mainContainer = document.querySelector('#main-container');
+const thanksContainer = document.querySelector(".thanks-container");
+const submitButton = document.getElementById('submit');
+const rateAgain = document.getElementById("rate-again");
+const rates = document.querySelectorAll(".btn");
+
+// add an event listener to submit button 
+submitButton.addEventListener("click", () =>{
+    thanksContainer.classList.remove('hidden');
+    mainContainer.style.display = "none";
+});
+
+// add an event listener to the thanks-container submit button
+
+rateAgain.addEventListener('click', () =>{
+    thanksContainer.classList.add('hidden');
+    mainContainer.style.display = "block";
+})
+
+rates.forEach((rate =>{
+    rate.addEventListener('click', () =>{
+        rating.innerHTML = rate.innerHTML;
+    })
+}))
+
+
+
